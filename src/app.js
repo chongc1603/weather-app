9b9e69b99f0995007f2b9da4f8ee8ec46eb5387c
chongc1603 <chongc1603@users.noreply.github.com>
2023-01-03 06:53:06 +1100

    Added icon update functionality using API

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let imageElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -36,10 +37,17 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt);
+  let imageId = response.data.weather[0].icon;
+  imageElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${imageId}@2x.png`
+  );
+  imageElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "11dd0fd4b3ac75eb75e9eb8f78c5fe37";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
