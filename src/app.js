bcea9965074e201f2abb19bb2998039a4162c86a
chongc1603 <chongc1603@users.noreply.github.com>
2023-01-12 07:12:01 +1100

    Added JS for the weather forecast

--- src/app.js
+++ src/app.js
@@ -22,6 +22,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="forecast-day">${day}</div>
+          <img
+          alt=""
+          src="http://openweathermap.org/img/wn/10d@2x.png"
+          width="42px"
+          />
+          <div class="forecast-temperatures">
+            <span class="forecast-temperature-max">18° </span>
+            <span class="forecast-temperature-min">12° </span>
+          </div>
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -93,3 +121,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
